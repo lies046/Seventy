c9e407b115bce6e00663f7c39a6798355af73971
lies046 <skytree.skysky@gmail.com>
2020-05-02 19:50:36 +0900

    Modified Views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,8 +12,6 @@ require("bootstrap");
 require("@fortawesome/fontawesome-free");
 require("jquery");
 require('preview.js');
-require('geolocation.js');
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
