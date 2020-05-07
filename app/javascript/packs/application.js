ad236e252f250eb877247d46139b3bbd43f18135
lies046 <skytree.skysky@gmail.com>
2020-05-07 13:06:39 +0900

    Add Geolocation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ require("bootstrap");
 require("@fortawesome/fontawesome-free");
 require("jquery");
 require('preview.js');
+require('location.js');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
