eac26511865014d900d9aff4460ef6a23f03e11b
lies046 <skytree.skysky@gmail.com>
2020-05-29 19:33:06 +0900

    Modified Application.jp

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,15 +6,15 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-// import 'bootstrap'
-// import '../src/application.scss'
 require("bootstrap");
 require("@fortawesome/fontawesome-free");
 require("jquery");
 require('preview.js');
 require('location.js');
 require('time.js');
-import 'stylesheets/application'
+import 'stylesheets/application.scss'
+import 'stylesheets/application.scss'
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
