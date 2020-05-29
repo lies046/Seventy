5a5bfd4513c587f2bd691a71796de7fff56b902d
lies046 <skytree.skysky@gmail.com>
2020-05-29 20:08:24 +0900

    Modified Application.html

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,9 +12,6 @@ require("jquery");
 require('preview.js');
 require('location.js');
 require('time.js');
-import 'stylesheets/application.scss'
-import 'stylesheets/application.scss'
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
